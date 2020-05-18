219e069f28f0b5163e8a57faaa733fb878bd6da8
Sicheng Zeng <sicheng.zeng.97@gmail.com>
2020-05-18 03:30:48 +0000

    host good

--- server_host.py
+++ server_host.py
@@ -2,39 +2,46 @@
 # Host a server that broadcasts mouse commands, CPU will connect to receive mouse commands
 
 import socket
-import sys
+import random
+import time
 
-HOST = "" #'127.0.0.1'
 PORT = 27519
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Starting up on port %s" %PORT)
 
-# Bind the socket to the port
-print("Starting up on %s port %s" %(HOST, PORT))
-sock.bind((HOST, PORT))
-sock.listen(1)
+# Create a UDP/IP socket
+s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+s.bind(('', PORT))
+s.listen(1)
+
+def get_random_direction():
+    r_num = random.randint(0, 4)
+    if r_num == 0:
+        return "L"
+    elif r_num == 1:
+        return "R"
+    elif r_num == 2:
+        return "U"
+    else:
+        return "D"
 
 while True:
+
     # Wait for a connection
     print("Waiting for a connection")
-    connection, client_address = sock.accept()
+    connection, client_address = s.accept()
+    print("Connection from " + str(client_address))
 
     try: 
-        print("Connection from", client_address)
-
-        # Receive the data in small chunks and retransmit it
         while True:
-            data = connection.recv(1024)
-            if data:
-                print("Received '%s'" % data)
-                print("Sending data back to the client")
-                connection.sendall(data)
-            else:
-                print("No more data from", client_address)
-                break
+            data_send = get_random_direction()
+            print("Sending: %s" % data_send)
+            connection.sendall(data_send)
+            time.sleep(1)
+    except Exception as e:
+        print("Socket error: %s" % str(e))
     finally:
         # Clean up with connection
+        print("Cleaning up connection")
         connection.close()
-
 
