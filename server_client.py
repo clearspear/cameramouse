599b088f4ad8e42e62b82eff073d25425142098e
Sicheng Zeng <sicheng.zeng.97@gmail.com>
2020-05-17 20:31:06 -0700

    client good

--- server_client.py
+++ server_client.py
@@ -3,32 +3,23 @@
 
 import socket
 import sys
+import signal
 
-HOST = '127.0.0.1'
+HOST = "192.168.100.2"
 PORT = 27519
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect to the port
 print("Connecting to %s port %s" % (HOST, PORT))
+
+# Create a TCP/IP socket
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
 try:
-    # Send data
-    message = "This is the message. It will be repeated."
-    print("Sending %s" % message)
-    sock.sendall(message)
-
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(1024)
-        amount_received += len(data)
-        print("Received '%s'" % data)
-
+    while True:
+        data = sock.recv(1)
+        if len(data) > 0:
+            print("Received: %s" % data)
 finally:
     print("Closing socket")
     sock.close()
+
