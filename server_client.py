6295561475a12cc772de8df599def06bbc3b00d4
Sicheng Zeng <sicheng.zeng.97@gmail.com>
2020-05-17 20:56:38 -0700

    moves mouse

--- server_client.py
+++ server_client.py
@@ -4,6 +4,7 @@
 import socket
 import sys
 import signal
+import pyautogui
 
 HOST = "192.168.100.2"
 PORT = 27519
@@ -14,11 +15,28 @@ print("Connecting to %s port %s" % (HOST, PORT))
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
+def parse_command(s):
+    command = s[0]
+    val = int(s[1:])
+    if command == "L":
+        pyautogui.moveRel(-50, 0, duration=.2)
+    elif command == "R":
+        pyautogui.moveRel(50, 0, duration=.2)
+    elif command == "U":
+        pyautogui.moveRel(0, -50, duration=.2)
+    elif command == "D":
+        pyautogui.moveRel(0, 50, duration=.2)
+    else:
+        print("Bad parse")
+
 try:
     while True:
-        data = sock.recv(1)
-        if len(data) > 0:
-            print("Received: %s" % data)
+        data = ""
+        expected_len = 5
+        while len(data) < expected_len:
+            data += sock.recv(5)
+        print("Received: %s" % data)
+        parse_command(data)
 finally:
     print("Closing socket")
     sock.close()
