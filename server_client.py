256f2f1d13c773338992d607eddf9e9c5de30ebe
Sicheng Zeng <sicheng.zeng.97@gmail.com>
2020-06-03 13:48:16 -0700

    updated server code, moves according to model output

--- server_client.py
+++ server_client.py
@@ -5,6 +5,7 @@ import socket
 import sys
 import signal
 import pyautogui
+import re
 
 HOST = "192.168.100.2"
 PORT = 27519
@@ -15,26 +16,63 @@ print("Connecting to %s port %s" % (HOST, PORT))
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
+# Current mouse location as percentage of screen size
+# Note: top left is (0, 0)
+x = -1
+y = -1
+
+# How much movement
+sensitivity = 1
+
+# Current state
+# 0 - move
+state = 0
+
+screen_x = pyautogui.size()[0]
+screen_y = pyautogui.size()[1]
+
 def parse_command(s):
-    command = s[0]
-    val = int(s[1:])
-    if command == "L":
-        pyautogui.moveRel(-50, 0, duration=.2)
-    elif command == "R":
-        pyautogui.moveRel(50, 0, duration=.2)
-    elif command == "U":
-        pyautogui.moveRel(0, -50, duration=.2)
-    elif command == "D":
-        pyautogui.moveRel(0, 50, duration=.2)
-    else:
-        print("Bad parse")
+    global x, y
+
+    if len(s) < 5:
+        return
+    if not s.startswith("[array"):
+        return
+
+    # Parse from ugly input string
+    box = re.sub('[^0-9,.]','', s)
+    box = box.split(",")
+    box = [float(coord) for coord in box]
+
+    # Use one hand only
+    box = box[0:4] 
+    print(box)
+
+    # Boxes are [top, left, bottom, right]
+    newx = (box[1] + box[3]) / 2
+    newy = (box[0] + box[2]) / 2
+
+    print(x, y, newx, newy)
+
+    # Move mouse
+    if x != -1:
+        x_diff = (newx - x) * screen_x
+        y_diff = (newy - y) * screen_y
+        print(x_diff, y_diff)
+        pyautogui.move(x_diff, y_diff)
+
+    # Set new state
+    x = newx
+    y = newy
 
 try:
     while True:
         data = ""
-        expected_len = 5
-        while len(data) < expected_len:
-            data += sock.recv(5)
+        while True:
+            c = sock.recv(1)
+            if c == '\n':
+                break
+            data += c
         print("Received: %s" % data)
         parse_command(data)
 finally:
