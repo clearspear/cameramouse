3295cc76fff4d33db98eeb26c466d42d7c14492e
Sicheng Zeng <sicheng.zeng.97@gmail.com>
2020-06-03 23:01:10 +0000

    displays the box and box center now

--- classify.py
+++ classify.py
@@ -32,12 +32,56 @@ import sys
 
 Category = collections.namedtuple('Category', ['id', 'score'])
 
+def clamp(v, l, r):
+    return max(min(v, r), l)
+
 # Draw stuff to the screen
 def generate_svg(size, text_lines):
+    font_size = '20'
+    red = svgwrite.rgb(255,0,0,'%')
+    grey = svgwrite.rgb(15,15,15,'%')
+
     dwg = svgwrite.Drawing('', size=size)
-    for y, line in enumerate(text_lines, start=1):
-      dwg.add(dwg.text(line, insert=(11, y*20+1), fill='black', font_size='20'))
-      dwg.add(dwg.text(line, insert=(10, y*20), fill='white', font_size='20'))
+
+    dwg.add(dwg.text("Boxes:", insert=(11, 21), fill='white', font_size=font_size))
+
+    x = size[0]
+    y = size[1]
+
+    # Draw boxes and text
+    box_i = 0
+    for box in text_lines:
+        top = clamp(int(box[0] * x), 0, x)
+        left = clamp(int(box[1] * y), 0, y)
+        bottom = clamp(int(box[2] * x), 0, x)
+        right = clamp(int(box[3] * y), 0, y)
+
+        # Write text
+        dwg.add(dwg.text(((top, left), (bottom, right)), 
+                         insert=(11, 20*(box_i+2) + 1), 
+                         fill='white', 
+                         font_size=font_size))
+
+        # Draw box
+        if box_i == 0:
+            color=red
+        else:
+            color=grey
+        dwg.add(dwg.rect((left, top), (right-left, bottom-top),
+                         stroke=color,
+                         fill='none'))
+
+        # Draw circle
+        if box_i == 0:
+            x_avg = int((left + right)/2)
+            y_avg = int((top + bottom)/2)
+            dwg.add(dwg.circle(center=(x_avg, y_avg),
+                               r=10,
+                               stroke=red,
+                               fill='red'))
+
+        box_i += 1
+
     return dwg.tostring()
 
 # Print output to stdout
@@ -56,12 +100,12 @@ def get_output(interpreter):
     print(hand_detections)
     sys.stdout.flush()
 
-    return
+    return hand_detections
 
 def main():
 
     # Flag to also show video 
-    show_display = False
+    show_display = True
 
     # Model path parameter
     model_path = '/home/mendel/handdetection_ssdmobilenetv1.tflite'
@@ -90,7 +134,7 @@ def main():
       end_time = time.monotonic()
 
       if show_display:
-          return generate_svg(src_size, "helloooo")
+          return generate_svg(src_size, results)
       return
 
     result = gstreamer.run_pipeline(user_callback,
